483200ee8e5d8a5d0516d20f5383ed44242ad18f
nc-kpm <kpm@netcompany.com>
2022-05-19 21:25:34 +0200

    working time for route

--- Controllers/API/RouteController.cs
+++ Controllers/API/RouteController.cs
@@ -8,6 +8,7 @@ using TelstarRoutePlanner.Controllers.API.Response_Models;
 using TelstarRoutePlanner.Data;
 using TelstarRoutePlanner.Extensions.RoutePlanner;
 using TelstarRoutePlanner.Models;
+using Route = TelstarRoutePlanner.Extensions.RoutePlanner.Route;
 
 namespace TelstarRoutePlanner.Controllers.API
 {
@@ -17,11 +18,14 @@ namespace TelstarRoutePlanner.Controllers.API
     {
         private readonly RoutePlanner _routePlanner;
         private readonly ApplicationDbContext _context;
+        private readonly List<Segment> _segments;
 
         public RouteController(ApplicationDbContext context)
         {
             _context = context;
             _routePlanner = new RoutePlanner(_context);
+            _segments = _context.GetSegments().ToList();
+
         }
 
         [Route("api/routes")]
@@ -31,7 +35,36 @@ namespace TelstarRoutePlanner.Controllers.API
             City cityA = _context.GetCities().Where(city => city.Name == getRoutesRequest.from).Single();
             City cityB = _context.GetCities().Where(city => city.Name == getRoutesRequest.to).Single();
 
-            var routes = _routePlanner.AllPaths(cityA, cityB);
+            List<Route> routes = _routePlanner.AllPaths(cityA, cityB);
+
+            // iterate through each segment and get the cost
+            // if the we are the provider of the route we get the cost from the database
+            // else we ask the other providers for the cost
+            foreach(Route route in routes)
+            {
+                List<City> cities = route.GetCities();
+
+                int totalCost = 0;
+
+                for (int i = 0; i < cities.Count - 1; i++)
+                {
+                    City city1 = cities[i];
+                    City city2 = cities[i + 1];
+
+                    Segment segment = this._segments.FirstOrDefault(s => (s.City1ID == city1.ID && s.City2ID == city2.ID) || (s.City1ID == city2.ID && s.City2ID == city1.ID));
+
+                    if (segment != null)
+                    {
+                        totalCost += segment.Cost ?? 0;
+                    }
+
+
+                }
+
+                route.setTime(totalCost);
+            }
+
+
             var routesJson = JsonSerializer.Serialize(routes);
 
             return Ok(routesJson);
--- Extensions/RoutePlanner/Route.cs
+++ Extensions/RoutePlanner/Route.cs
@@ -5,10 +5,26 @@ namespace TelstarRoutePlanner.Extensions.RoutePlanner
 {
     public class Route
     {
+        private int _time = 0;
         private List<City> cities;
         public Route(List<City> cities)
         {
             this.cities = cities;
+        }
+
+        public List<City> GetCities()
+        {
+            return cities;
+        }
+
+        public void setTime(int time)
+        {
+            _time = time;
+        }
+
+        public int getTime()
+        {
+            return _time;
         }
     }
 }
