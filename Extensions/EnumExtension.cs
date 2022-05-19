b6c98cb63dec7d990b3a4447ec65a812e1802bf7
Thomas Slatter <thomas.slatter@netcompany.com>
2022-05-19 21:39:30 +0100

    More efficient functionality for enum values

---
+++ Extensions/EnumExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace TelstarRoutePlanner.Extensions
+{
+    public class EnumExtension<TEnum>
+    {
+        public static string GetDescription(TEnum val)
+        {
+            FieldInfo fi = typeof(TEnum).GetField(val.ToString());
+            DescriptionAttribute[] attrs = fi.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+            return attrs != null && attrs.Any() ? attrs[0].Description : val.ToString();
+        }
+
+        public static List<TEnum> GetEnumValsList()
+        {
+            return Enum.GetValues(typeof(TEnum)).Cast<TEnum>().ToList();
+        }
+
+        public static List<SelectListItem> CastEnumSelectList()
+        {
+            return GetEnumValsList().Select(x => new SelectListItem(GetDescription(x), x.ToString())).ToList();
+        }
+
+        public static List<SelectListItem> CastEnumSelectList(TEnum val)
+        {
+            return GetEnumValsList().Select(x => new SelectListItem(GetDescription(x), x.ToString(), x.Equals(val))).ToList();
+        }
+    }
+}
