8f82424fea17baeb77e53792decd493d05399b5b
Thomas Slatter <thomas.slatter@netcompany.com>
2022-05-19 10:12:22 +0100

    Set up API class for routes

---
+++ Controllers/API/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TelstarRoutePlanner.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RouteController : ControllerBase
+    {
+        [AllowAnonymous]
+        public IActionResult GetRoute()
+        {
+            return Ok("API test succeeded")
+        }
+    }
+}
